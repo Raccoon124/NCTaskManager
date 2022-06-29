6a513a6e6c6aa3624b013822ea8ba524cd16357e
Alberto <alsanchezbeltran@gmail.com>
2022-06-29 17:50:25 -0500

    evaluation 1; Task 1 complete

---
+++ src/main/java/mx/tc/j2se/evaluation/Circle.java
@@ -0,0 +1,43 @@
+package mx.tc.j2se.evaluation;
+
+public class Circle {
+
+    private int radius;
+    private double area;
+
+    public Circle() {
+        this.radius = 1;
+    }
+
+    public Circle(int radius) {
+
+        if (radius <= 0) {
+            throw new IllegalArgumentException("the radius was invalid");
+        }
+        this.radius = radius;
+    }
+
+    public double getArea() {
+
+        this.area = Math.PI * this.radius * this.radius;
+        return area;
+
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+
+        if (radius <= 0) {
+            throw new IllegalArgumentException("the radius was invalid");
+        }
+        this.radius = radius;
+    }
+}
---
+++ src/main/java/mx/tc/j2se/evaluation/Main.java
@@ -0,0 +1,19 @@
+package mx.tc.j2se.evaluation;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        Circle circulo1 = new Circle(5);
+        circulo1.setRadius(2);
+        System.out.println(circulo1.getRadius());
+        System.out.println(circulo1.getArea());
+
+
+
+    }
+
+
+}
